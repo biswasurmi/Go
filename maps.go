188aaa2a10ee5fe7d98e2eefa5aea506c8db104d
urmi <urmi@appscode.com>
2025-06-11 15:41:58 +0600

    c3 main : add variable, maps, function, controlstructure, array in one go

---
+++ maps.go
@@ -0,0 +1,39 @@
+package main
+import "fmt"
+func main() {
+	// the below code will show error as the map is not initialized, we must need to initialized it first then assiging
+	/*var x map[string]int
+	x["key"] = 10
+	fmt.Println(x["key"])*/
+	x := make(map[string]int) // initialization
+	x["key"] = 10 // assigning
+	x["abc"] = 20
+	x["lmn"] = 30
+	//fmt.Print(x["key"])
+	fmt.Println(x)
+	delete(x, "key") // deletion
+	fmt.Println(x) // full print of the map
+	fmt.Println(x["ab"]) // return 0 for unassigned value
+	name, ok := x["lmn"] // if value is present then ok is true else false
+	fmt.Println(name, ok) 
+	if name, ok := x["ab"]; ok {
+		fmt.Println(name, ok)
+	}
+	elements := map[string]string {
+		"h" : "hydro",
+		"he" : "helium",
+		"li" : "lithium",
+	}
+	fmt.Println(elements)
+	newEle := map[string]map[string]int{
+		"h": map[string]int{
+			"val" : 10, 
+			"val1" : 11,
+		},
+
+	}
+	fmt.Println(newEle)
+	if el, ok := newEle["h"]; ok {
+		fmt.Println(el["val"], el["val1"])
+	}
+}
---
+++ variable.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+//globalVariable := 10 // will show error as := this is only for local function
+var globalVariable = 10 // you must use var for global variable
+func main(){
+	var x string // var variable_name variable_type
+	x = "Hlw world!"
+	fmt.Println(x)
+	var y string = "hi world!@"
+	fmt.Println(y)
+	var z string = x + y
+	fmt.Println(z)
+	fmt.Println(x == y)
+	p := 5 // : is for assigning value and no need to declare data type
+	fmt.Println(p)
+	var q = "Hlw world" // you can't use := here as you have declared 'var' here but no need to declare data type
+	fmt.Println(q)
+	myName := "Urmi"
+	fmt.Println("My name is ", myName)
+	fmt.Println(globalVariable)
+	f() // calling another function
+	const con = 10
+	// con = 12 // error as assigning is not allowed for constants
+
+	var(
+		a = 5
+		b string = "hlw"
+	)
+	fmt.Println(a, b)
+	//example()
+	celToFar()
+}
+func f(){
+	fmt.Println(globalVariable)
+}
+func example(){
+	var input float64
+	fmt.Print("enter a float number ")
+	fmt.Scanf("%f", &input)
+	output := input * 2
+	fmt.Println(output)
+	output += 1
+	fmt.Println(output)
+}
+func celToFar(){
+	var far float64
+	fmt.Scanf("%f", &far)
+	var cel float64 = ((far - 32) * ( 5.0/ 9))
+	fmt.Println(cel)
+
+}
