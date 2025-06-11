ed6ffdfd6fbdc0092430d15fa3cb2a3320dfbeda
urmi <urmi@appscode.com>
2025-06-11 11:59:30 +0600

    C1 main: add demo, number, string file

---
+++ string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Urmi") // print with a newline
+	fmt.Println("hello world"[1]) // print asci value of string index [1]
+	fmt.Printf("%c\n", "hello world"[1]) // print the character itself of the string
+	fmt.Println("hello " + "world") // concatenate
+}
